9bd14fe481109a8b81a8a023b8be998af0ec5fc0
variyant <88224509+variyant@users.noreply.github.com>
2023-11-15 09:53:40 -0500

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import PostCard from '../components/PostCard';
+
+function Home({ posts }) {
+    // posts is an array of post objects pulled from the back end. (Passed down from the parent as a prop.)
+    function renderPost() {
+        return (
+            // When you're mapping posts, you're iterating through the array of objects and performing whatever
+            // functional code you're writing in. In this case, it's rendering in a subcomponent and passing the
+            // individual post data into the subcomponent.
+            posts.map(post => <PostCard post={post} />)
+        )
+    }
+
+    return (
+        <div>
+            {renderPost()}
+        </div>
+    )
+}
+
+export default Home;
