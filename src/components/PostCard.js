5dd11c74a4a0074ed3b33aee8256a06d9dc8a188
variyant <88224509+variyant@users.noreply.github.com>
2023-11-20 04:32:29 -0500

    Update PostCard.js

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -2,12 +2,20 @@ function PostCard({ post }) {
     const { pfp, author, date, message, liked } = post;
 
     return (
-        <div>
-            {pfp}
-            {author}
-            {date}
-            {message}
-            {liked}
+        <div className="PostCard">
+            <div className="post-bar">
+                <img src={pfp} id="pfp" alt="Profile Picture" />
+                <p>{author}</p>
+                <p>{date}</p>
+            </div>
+            <div id="msg">
+                <p>{message}</p>
+            </div>
+            <div id="like">
+                <button>{liked ? "yes" : "no"}</button>
+            </div>
         </div>
     )
 }
+
+export default PostCard;
